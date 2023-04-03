b3f9c4b8dea644ab6ed18fedccf2533a27962543
Amir0701 <amirusmonov2002@gmail.com>
2023-04-03 22:47:28 +0300

    objects recycler setting

--- app/src/main/java/com/example/tenant/ui/view/ObjectsAdapter.kt
+++ app/src/main/java/com/example/tenant/ui/view/ObjectsAdapter.kt
@@ -3,14 +3,19 @@ package com.example.tenant.ui.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tenant.R
+import com.example.tenant.data.model.Obbject
+import com.example.tenant.data.model.ObjectStatus
 
 class ObjectsAdapter: RecyclerView.Adapter<ObjectsAdapter.ViewHolder>() {
-    var objectsList: List<ObjectsAdapter> = ArrayList()
+    var objectsList: List<Obbject> = ArrayList()
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
+        val nameTextView: TextView = itemView.findViewById(R.id.cardObjectName)
+        val categoryTextView: TextView = itemView.findViewById(R.id.cardObjectCategory)
+        val statusTextView: TextView = itemView.findViewById(R.id.cardObjectStatus)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -19,10 +24,41 @@ class ObjectsAdapter: RecyclerView.Adapter<ObjectsAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentObject = objectsList[position]
 
+        holder.nameTextView.text = currentObject.name
+        holder.categoryTextView.text = currentObject.categoryId.toString()
+        when (currentObject.objectStatus) {
+            ObjectStatus.FREE -> {
+                holder.statusTextView.text = "Свободный"
+                holder.statusTextView.setTextColor(holder.itemView.resources.getColor(R.color.green))
+            }
+            ObjectStatus.IN_TENANT -> {
+                holder.statusTextView.text = "В аренде"
+                holder.statusTextView.setTextColor(holder.itemView.resources.getColor(R.color.red))
+            }
+            else -> {
+                holder.statusTextView.text = "В ремонте"
+                holder.statusTextView.setTextColor(holder.itemView.resources.getColor(R.color.blue))
+            }
+        }
+
+        holder.itemView.setOnClickListener {
+            objectItemClickListener?.onItemClick(currentObject)
+        }
     }
 
     override fun getItemCount(): Int {
         return objectsList.size
+    }
+
+    interface ObjectItemClickListener{
+        fun onItemClick(obbject: Obbject)
+    }
+
+    private var objectItemClickListener: ObjectItemClickListener? = null
+
+    fun setObjectItemClickListener(objectItemClickListener: ObjectItemClickListener){
+        this.objectItemClickListener = objectItemClickListener
     }
 }
--- app/src/main/java/com/example/tenant/ui/view/ObjectsFragment.kt
+++ app/src/main/java/com/example/tenant/ui/view/ObjectsFragment.kt
@@ -1,15 +1,21 @@
 package com.example.tenant.ui.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tenant.R
+import com.example.tenant.data.model.Obbject
+import com.example.tenant.data.model.ObjectStatus
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ObjectsFragment : Fragment() {
+    private lateinit var objectsRecyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +29,27 @@ class ObjectsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val addNewObjectButton: FloatingActionButton = view.findViewById(R.id.add_new_object)
+        objectsRecyclerView = view.findViewById(R.id.objectsRecycler)
+        initRecycler()
 
         addNewObjectButton.setOnClickListener {
             findNavController().navigate(R.id.action_objectsFragment_to_arendatorFragment)
         }
+    }
+
+    private fun initRecycler(){
+        val objec1 = Obbject(1, "Квартира", 1, ObjectStatus.IN_TENANT, 32.0, "")
+        val objec2 = Obbject(2, "Дача", 2, ObjectStatus.FREE, 44.0, "")
+        val list = listOf(objec1, objec2)
+        val adapter = ObjectsAdapter()
+        adapter.objectsList = list
+        objectsRecyclerView.adapter = adapter
+        objectsRecyclerView.layoutManager = LinearLayoutManager(context)
+        adapter.setObjectItemClickListener(object: ObjectsAdapter.ObjectItemClickListener{
+            override fun onItemClick(obbject: Obbject) {
+                val intent = Intent(activity, ChosenObjectActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
