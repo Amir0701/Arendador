5dde0be80bb828d1b32115891bf87de9495f82b4
Amir0701 <amirusmonov2002@gmail.com>
2023-04-07 22:52:18 +0300

    feature/add object from ui

--- app/src/main/java/com/example/tenant/ui/view/NewObjectFragment.kt
+++ app/src/main/java/com/example/tenant/ui/view/NewObjectFragment.kt
@@ -8,13 +8,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.EditText
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.tenant.R
+import com.example.tenant.data.model.Obbject
+import com.example.tenant.data.model.ObjectStatus
 import com.example.tenant.ui.model.MainActivityViewModel
+import com.google.android.material.textfield.TextInputLayout
 
 class NewObjectFragment : Fragment() {
     private lateinit var mainActivityViewModel: MainActivityViewModel
     private lateinit var categoryList: AutoCompleteTextView
+    private lateinit var addObjectButton: Button
+    private lateinit var nameEditText: EditText
+    private lateinit var squareEditText: EditText
+    private lateinit var addressEditText: EditText
+    private lateinit var nameInputLayout: TextInputLayout
+    private lateinit var categoryInputLayout: TextInputLayout
+
+    private val categoryNames = mutableListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +41,19 @@ class NewObjectFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         categoryList = view.findViewById(R.id.categoryList)
+        addObjectButton = view.findViewById(R.id.addObjectButton)
+        nameEditText = view.findViewById(R.id.objectNameEditText)
+        squareEditText = view.findViewById(R.id.squareEditText)
+        addressEditText = view.findViewById(R.id.addressEditText)
+        nameInputLayout = view.findViewById(R.id.nameInputLayout)
+        categoryInputLayout = view.findViewById(R.id.categoriesInputLayout)
+
+        addObjectButton.setOnClickListener {
+            getData()?.let {
+                mainActivityViewModel.addObject(it)
+                findNavController().navigate(R.id.action_newObjectFragment2_to_objectsFragment)
+            }
+        }
     }
 
     override fun onStart() {
@@ -37,7 +64,6 @@ class NewObjectFragment : Fragment() {
     }
 
     private fun observeCategories(){
-        val categoryNames = mutableListOf<String>()
         mainActivityViewModel.categoryLiveData.observe(viewLifecycleOwner, Observer {
             it?.let {list->
                 Log.i("categ", list.size.toString())
@@ -48,6 +74,52 @@ class NewObjectFragment : Fragment() {
                 categoryList.setAdapter(adapter)
             }
         })
+    }
+
+    private fun getData(): Obbject?{
+        val name:String? = if(nameEditText.text.toString().isNotEmpty()){
+            nameInputLayout.helperText = ""
+            nameEditText.text.toString()
+        }else{
+            nameInputLayout.helperText = requireContext().resources.getString(R.string.fillIn)
+            null
+        }
+
+        val idCat:Int? = if(categoryList.text.toString() != "Вид объекта"){
+            categoryInputLayout.helperText = ""
+            var id = 0
+            mainActivityViewModel.categoryLiveData.value?.let { list->
+                list.forEach{category->
+                    if(category.name == categoryList.text.toString()){
+                        id = category.id
+                    }
+                }
+            }
+            id
+        }
+        else{
+            categoryInputLayout.helperText = requireContext().resources.getString(R.string.choose_category)
+            null
+        }
+
+        val address:String? = if(nameEditText.text != null){
+            nameEditText.text.toString()
+        }
+        else{
+            null
+        }
+
+        val square: Double? = if(squareEditText.text.toString().isNotEmpty()){
+            squareEditText.text.toString().toDouble()
+        }
+        else{
+            null
+        }
+
+        if(name != null && idCat != null){
+            return Obbject(0, name, idCat, ObjectStatus.FREE, square, address)
+        }
+        return null
     }
 
     companion object {
