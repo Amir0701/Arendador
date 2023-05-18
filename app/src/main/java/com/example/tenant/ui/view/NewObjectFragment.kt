c14b4cbbb98ddc6059f0d5af6087c90473f2a9d6
Amir0701 <amirusmonov2002@gmail.com>
2023-05-18 22:31:53 +0300

    feature/add path util and choose image

--- app/src/main/java/com/example/tenant/ui/view/NewObjectFragment.kt
+++ app/src/main/java/com/example/tenant/ui/view/NewObjectFragment.kt
@@ -1,6 +1,12 @@
 package com.example.tenant.ui.view
 
+import android.Manifest
+import android.app.Activity
+import android.content.ClipData
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.FileUtils
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +16,9 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.net.toFile
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -17,7 +26,12 @@ import com.example.tenant.R
 import com.example.tenant.data.model.Obbject
 import com.example.tenant.data.model.ObjectStatus
 import com.example.tenant.ui.model.MainActivityViewModel
+import com.example.tenant.util.RealPathUtil
 import com.google.android.material.textfield.TextInputLayout
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.io.InputStreamReader
 
 class NewObjectFragment : Fragment() {
     private lateinit var mainActivityViewModel: MainActivityViewModel
@@ -28,10 +42,14 @@ class NewObjectFragment : Fragment() {
     private lateinit var addressEditText: EditText
     private lateinit var nameInputLayout: TextInputLayout
     private lateinit var categoryInputLayout: TextInputLayout
+    private lateinit var addImageButton: Button
+    private var imageByteArray: ByteArray? = null
 
     private val args: NewObjectFragmentArgs by navArgs()
 
     private val categoryNames = mutableListOf<String>()
+
+    private val CHOOSE_IMAGE = 101
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,12 +68,17 @@ class NewObjectFragment : Fragment() {
         addressEditText = view.findViewById(R.id.addressEditText)
         nameInputLayout = view.findViewById(R.id.nameInputLayout)
         categoryInputLayout = view.findViewById(R.id.categoriesInputLayout)
+        addImageButton = view.findViewById(R.id.addImageButton)
 
         addObjectButton.setOnClickListener {
             getData()?.let {
                 mainActivityViewModel.addObject(it)
                 findNavController().navigate(R.id.action_newObjectFragment2_to_objectsFragment)
             }
+        }
+
+        addImageButton.setOnClickListener {
+            chooseImage()
         }
 
         setObject()
@@ -147,10 +170,47 @@ class NewObjectFragment : Fragment() {
 
         if(name != null && idCat != null){
             args.editObject?.let {
-                return Obbject(it.id, name, idCat, it.objectStatus, square, address)
+                if(imageByteArray != null)
+                    return Obbject(it.id, name, idCat, it.objectStatus, square, address, imageByteArray)
+                else
+                    return Obbject(it.id, name, idCat, it.objectStatus, square, address)
             }
-            return Obbject(0, name, idCat, ObjectStatus.FREE, square, address)
+            return Obbject(0, name, idCat, ObjectStatus.FREE, square, address, imageByteArray)
         }
         return null
+    }
+
+    private fun chooseImage(){
+        if (ContextCompat.checkSelfPermission(
+                requireContext().applicationContext,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            startActivityForResult(Intent.createChooser(intent, "SELECT PHOTO"), CHOOSE_IMAGE)
+        } else {
+            ActivityCompat.requestPermissions(
+                (activity as MainActivity),
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == CHOOSE_IMAGE && resultCode == Activity.RESULT_OK){
+            if(data != null){
+                val uri = data.data
+                val file = File(RealPathUtil.getRealPath(requireContext(), uri))
+                val fileInputStream = FileInputStream(file)
+                imageByteArray = ByteArray(file.length().toInt())
+                fileInputStream.read(imageByteArray)
+                Log.i("image", imageByteArray.toString())
+            }
+        }
     }
 }
